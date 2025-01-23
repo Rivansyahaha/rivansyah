ec7df0023d7c5c2b783bd27f3c33c3eeff4101f0
Rivansyahaha <2rivanca2006@gmail.com>
2025-01-23 15:34:28 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+// const nama = "Rivansyah";
+// let usia = 18;
+
+// let biodata = document.getElementById('biodata');
+
+// function generateBiodata() {
+//     let generasi;
+
+//     if (usia >= 10 && usia <= 18) {
+//         generasi = "remaja";
+//     } 
+//     else if (usia >= 19 && usia <= 59) {
+//         generasi = "dewasa";
+//     } 
+//     else if (usia >= 60 && usia <= 70) {
+//         generasi = "lansia";
+//     } 
+//     else if (usia >= 71) {
+//         generasi = "wah biasanya udah mati sih bro";
+//     } 
+//     else if (usia >= 5 && usia <= 9) {
+//         generasi = "anak-anak";
+//     } 
+//     else {
+//         generasi = "balita";
+//     } 
+//     return biodata.innerHTML = generasi;
+// }
+
+// console.log(nama);
+// console.log(usia);
+
+// generateBiodata();
